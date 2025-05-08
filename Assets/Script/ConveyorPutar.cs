843bead692efaf3a47ba8bd5324ff1f140870889
Limoraaa <mahardikalintang97@gmail.com>
2025-05-08 10:52:14 +0700

    Empat
    alusken

---
+++ Assets/Script/ConveyorPutar.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class ConveyorPutar : MonoBehaviour
+{
+    [Header("Conveyor Settings")]
+    public Vector3 moveDirection = Vector3.forward;
+    public float moveSpeed = 2f;
+
+    private bool isRunning = true;
+
+    public void ToggleConveyor()
+    {
+        isRunning = !isRunning;
+        Debug.Log("Conveyor is now " + (isRunning ? "Running" : "Stopped"));
+    }
+
+    private void OnCollisionStay(Collision collision)
+    {
+        if (!isRunning) return;
+
+        Rigidbody rb = collision.rigidbody;
+
+        if (rb != null && !rb.isKinematic)
+        {
+            rb.linearVelocity = moveDirection.normalized * moveSpeed;
+        }
+    }
+
+}
